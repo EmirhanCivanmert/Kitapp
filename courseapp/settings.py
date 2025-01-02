cd21f4b2fea04d5507c0358e7dfd47782a420a4f
Emirhan Civanmert <emirhancivanmert.sd@gmail.com>
2025-01-02 17:07:33 +0300

    vs15

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "static",
 ]
 
 MEDIA_ROOT = BASE_DIR / "uploads"
